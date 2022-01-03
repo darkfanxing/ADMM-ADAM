89d867d4602e34f14f472af6125a821066ff4d88
darkfanxing <darkfanxing@gmail.com>
2022-01-03 15:37:49 +0800

    Add reference into src/utils/adjust_image/adjust_image_constrast

--- src/utils/adjust_image/adjust_image_contrast.py
+++ src/utils/adjust_image/adjust_image_contrast.py
@@ -1,10 +1,9 @@
 from cv2 import createCLAHE, merge
 
+# Reference: https://blog.csdn.net/qq_43426908/article/details/121221054
 def adjust_image_contrast(image):
     clahe = createCLAHE(clipLimit=1, tileGridSize=(8, 8))
-
-    clahe_B = clahe.apply(image[:, :, 0])
-    clahe_G = clahe.apply(image[:, :, 1])
-    clahe_R = clahe.apply(image[:, :, 2])
-
-    return merge((clahe_R, clahe_G, clahe_B))
+    clahe_b = clahe.apply(image[:, :, 0])
+    clahe_g = clahe.apply(image[:, :, 1])
+    clahe_r = clahe.apply(image[:, :, 2])
+    return merge((clahe_r, clahe_g, clahe_b))
